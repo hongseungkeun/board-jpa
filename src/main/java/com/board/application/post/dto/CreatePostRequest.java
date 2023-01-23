204ca5cbaf79e42e0a21f391b5d64cf14a6bca15
hongseungkeun <eiziong1@gmail.com>
2023-01-23 18:41:13 +0900

    refactor: id에 @NotNull 추가

--- src/main/java/com/board/application/post/dto/CreatePostRequest.java
+++ src/main/java/com/board/application/post/dto/CreatePostRequest.java
@@ -3,8 +3,9 @@ package com.board.application.post.dto;
 import com.board.application.post.domain.Post;
 import com.board.application.user.domain.User;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record CreatePostRequest(@NotBlank String title, @NotBlank String content, Long id) {
+public record CreatePostRequest(@NotBlank String title, @NotBlank String content, @NotNull Long id) {
     public Post toPost(User user){
         return new Post(this.title, this.content, user);
     }
