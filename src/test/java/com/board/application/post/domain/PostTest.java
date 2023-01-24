6e26955cb99c66822c3ba76e9cf9b114d3f017d7
hongseungkeun <eiziong1@gmail.com>
2023-01-25 00:07:35 +0900

    test: ParameterizedTest, MethodSource를 사용한 테스트로 수정

--- src/test/java/com/board/application/post/domain/PostTest.java
+++ src/test/java/com/board/application/post/domain/PostTest.java
@@ -2,29 +2,38 @@ package com.board.application.post.domain;
 
 import com.board.application.user.domain.User;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PostTest {
 
     @DisplayName("제목은 빈값일 수 없다.")
-    @Test
-    void titleCannotNull(){
+    @ParameterizedTest
+    @MethodSource("blankOrNullStrings")
+    void titleCannotNull(String title) {
         User user = new User("홍승근", "취미", 25);
 
-        assertThrows(IllegalArgumentException.class, () -> new Post("", "안녕하세요", user));
-        assertThrows(IllegalArgumentException.class, () -> new Post(" ", "안녕하세요", user));
-        assertThrows(IllegalArgumentException.class, () -> new Post(null, "안녕하세요", user));
+        assertThrows(IllegalArgumentException.class, () -> new Post(title, "안녕하세요", user));
+        assertThrows(IllegalArgumentException.class, () -> new Post(title, "안녕하세요", user));
+        assertThrows(IllegalArgumentException.class, () -> new Post(title, "안녕하세요", user));
     }
 
     @DisplayName("글은 빈값일 수 없다")
-    @Test
-    void contentCannotNull(){
+    @ParameterizedTest
+    @MethodSource("blankOrNullStrings")
+    void contentCannotNull(String content){
         User user = new User("홍승근", "취미", 25);
 
-        assertThrows(IllegalArgumentException.class, () -> new Post("제목", "", user));
-        assertThrows(IllegalArgumentException.class, () -> new Post("제목", " ", user));
-        assertThrows(IllegalArgumentException.class, () -> new Post("제목", null, user));
+        assertThrows(IllegalArgumentException.class, () -> new Post("제목", content, user));
+        assertThrows(IllegalArgumentException.class, () -> new Post("제목", content, user));
+        assertThrows(IllegalArgumentException.class, () -> new Post("제목", content, user));
+    }
+
+    static Stream<String> blankOrNullStrings() {
+        return Stream.of("", " ", null);
     }
 }
--- src/test/java/com/board/application/user/domain/UserTest.java
+++ src/test/java/com/board/application/user/domain/UserTest.java
@@ -2,22 +2,31 @@ package com.board.application.user.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class UserTest {
 
     @DisplayName("이름은 빈값일 수 없다.")
-    @Test
-    void NameCannotNull(){
-        assertThrows(IllegalArgumentException.class, () -> new User("", "취미", 25));
-        assertThrows(IllegalArgumentException.class, () -> new User(" ", "취미", 25));
-        assertThrows(IllegalArgumentException.class, () -> new User(null, "취미", 25));
+    @ParameterizedTest
+    @MethodSource("blankOrNullStrings")
+    void NameCannotNull(String name){
+        assertThrows(IllegalArgumentException.class, () -> new User(name, "취미", 25));
+        assertThrows(IllegalArgumentException.class, () -> new User(name, "취미", 25));
+        assertThrows(IllegalArgumentException.class, () -> new User(name, "취미", 25));
     }
 
     @DisplayName("나이는 0살 이하일 수 없다")
     @Test
     void ageCannotZero(){
         assertThrows(IllegalArgumentException.class, () -> new User("홍승근", "취미", 0));
+    }
+
+    static Stream<String> blankOrNullStrings() {
+        return Stream.of("", " ", null);
     }
 }
