b1d5af1dfd09b749855d1612f934975f45c0ae89
hongseungkeun <eiziong1@gmail.com>
2023-01-25 00:12:29 +0900

    refactor: NullPointerException 방지를 위한 posts 초기화 추가

--- src/main/java/com/board/application/user/domain/User.java
+++ src/main/java/com/board/application/user/domain/User.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 import org.springframework.util.Assert;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -26,7 +27,7 @@ public class User extends BaseEntity {
     private Integer age;
 
     @OneToMany(mappedBy = "user")
-    private List<Post> posts;
+    private List<Post> posts = new ArrayList<>();
 
     public Long getId() {
         return id;
