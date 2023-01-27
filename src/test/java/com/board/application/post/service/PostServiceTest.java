aa3b9f10733707832836959016ede5c4bab8e488
hongseungkeun <eiziong1@gmail.com>
2023-01-27 18:45:20 +0900

    test: 공통 변수를 위한 @BeforeEach 추가

--- src/test/java/com/board/application/post/service/PostServiceTest.java
+++ src/test/java/com/board/application/post/service/PostServiceTest.java
@@ -9,9 +9,7 @@ import com.board.application.user.domain.User;
 import com.board.application.user.repository.UserRepository;
 import com.board.core.exception.PostNotFoundException;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -37,15 +35,23 @@ public class PostServiceTest {
     private UserRepository userRepository;
     @InjectMocks
     private PostService postService;
+    private Post samplePost;
+    private User sampleUserWithId;
+    private Post samplePostWithId;
+
+    @BeforeEach
+    void setUp(){
+        samplePost = new Post("제목", "안녕하세요", new User("홍승근", "취미", 25));
+        sampleUserWithId = new User(1L, "홍승근", "취미", 25);
+        samplePostWithId = new Post(1L, "제목", "안녕하세요", sampleUserWithId);
+    }
 
     @DisplayName("게시글을 페이징 조회할 수 있다.")
     @Test
     void getPostsTest() {
-        User user = new User("홍승근", "취미", 25);
-        Post post = new Post("제목", "안녕하세요", user);
         List<Post> posts = new ArrayList<>();
         for (int i = 0; i < 15; i++) {
-            posts.add(post);
+            posts.add(samplePost);
         }
         PageRequest request = PageRequest.of(1, 10);
         Page<Post> postPage = new PageImpl<>(posts, request, 10);
@@ -72,40 +78,34 @@ public class PostServiceTest {
         @DisplayName("게시물이 있다면 해당 게시물을 조회한다")
         @Test
         void getPostSuccessTest() {
-            User user = new User(1L, "홍승근", "취미", 25);
-            Post post = new Post(1L, "제목", "안녕하세요", user);
+            given(postRepository.findById(anyLong())).willReturn(Optional.of(samplePost));
 
-            given(postRepository.findById(anyLong())).willReturn(Optional.of(post));
-
-            assertThat(postService.findPostById(1L)).isEqualTo(post);
+            assertThat(postService.findPostById(1L)).isEqualTo(samplePost);
         }
     }
 
     @DisplayName("게시물을 생성할 수 있다")
     @Test
     void createPostTest() {
-        User user = new User(1L, "홍승근", "취미", 25);
-        Post post = new Post(1L, "제목", "안녕하세요", user);
         CreatePostRequest request = new CreatePostRequest("제목", "안녕하세요", 1L);
 
-        given(userRepository.findById(anyLong())).willReturn(Optional.of(user));
-        given(postRepository.save(any())).willReturn(post);
+        given(userRepository.findById(anyLong())).willReturn(Optional.of(sampleUserWithId));
+        given(postRepository.save(any())).willReturn(samplePostWithId);
+
         Long id = postService.createPost(request);
 
-        assertThat(id).isEqualTo(post.getId());
+        assertThat(id).isEqualTo(samplePostWithId.getId());
     }
 
     @DisplayName("게시물을 수정할 수 있다")
     @Test
     void updatePostTest() {
-        User user = new User(1L, "홍승근", "취미", 25);
-        Post post = new Post(1L, "제목", "안녕하세요", user);
         UpdatePostRequest request = new UpdatePostRequest("테스트", "안녕하세요 반갑습니다");
 
-        given(postRepository.findById(anyLong())).willReturn(Optional.of(post));
+        given(postRepository.findById(anyLong())).willReturn(Optional.of(samplePostWithId));
 
         postService.updatePost(1L, request);
 
-        assertThat(post.getContent()).isEqualTo(request.content());
+        assertThat(samplePostWithId.getContent()).isEqualTo(request.content());
     }
 }
