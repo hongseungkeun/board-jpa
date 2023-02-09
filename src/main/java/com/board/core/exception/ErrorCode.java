5dc99ea47864ff88bc32b272c4b5dcdd084c7bec
hongseungkeun <eiziong1@gmail.com>
2023-02-09 22:52:06 +0900

    refactor: 에러 상태 코드 추가

--- src/main/java/com/board/core/exception/ErrorCode.java
+++ src/main/java/com/board/core/exception/ErrorCode.java
@@ -5,7 +5,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시물이 존재하지 않습니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저가 존재하지 않습니다."),
-    LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "잘못된 비밀번호 입니다." );
+    EXIST_USER(HttpStatus.CONFLICT, "이미 존재하는 이메일 입니다."),
+    LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "잘못된 비밀번호 입니다." ),
+    AUTH_FAILED(HttpStatus.UNAUTHORIZED, "로그인 후 이용 가능합니다." );
 
     private final HttpStatus status;
     private final String message;
