7ab22ef69e2ffd6fb6c1aa57f31625319f9702e9
hongseungkeun <eiziong1@gmail.com>
2023-02-16 00:16:12 +0900

    refactor: 사용하지 않는 import 제거

--- src/main/java/com/board/application/post/dto/CreatePostRequest.java
+++ src/main/java/com/board/application/post/dto/CreatePostRequest.java
@@ -3,7 +3,6 @@ package com.board.application.post.dto;
 import com.board.application.post.domain.Post;
 import com.board.application.user.domain.User;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record CreatePostRequest(@NotBlank String title, @NotBlank String content) {
     public Post toPost(User user){
