5106ec4399529b262ef1cfd100f688cbc30072c7
hongseungkeun <eiziong1@gmail.com>
2023-03-02 22:13:02 +0900

    refactor: rest docs 적용

--- src/test/java/com/board/application/post/controller/PostControllerTest.java
+++ src/test/java/com/board/application/post/controller/PostControllerTest.java
@@ -3,27 +3,37 @@ package com.board.application.post.controller;
 import com.board.application.post.domain.Post;
 import com.board.application.post.dto.PostRequest;
 import com.board.application.post.repository.PostRepository;
+import com.board.application.user.controller.UserController;
 import com.board.application.user.domain.User;
 import com.board.application.user.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import java.util.List;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 public class PostControllerTest {
     @Autowired
-    private PostController postController;
+    private UserController userController;
     @Autowired
     private PostRepository postRepository;
     @Autowired
@@ -33,53 +43,175 @@ public class PostControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @BeforeEach
-    void setUp(){
-        User user = new User("홍승근", "취미", 25, "123@123.com", "123123");
-        User save = userRepository.save(user);
-        Post post1 = new Post("제목1", "안녕하세요", save);
-        Post post2 = new Post("제목2", "안녕하세요", save);
-        postRepository.save(post1);
-        postRepository.save(post2);
-    }
-
-    @DisplayName("게시글을 조회할 수 있다")
-    @Test
-    void getPostsTest() throws Exception {
-        mockMvc.perform(get("/posts"))
-                .andDo(print())
-                .andExpect(status().isOk());
+    @AfterEach
+    void deleteAll(){
+        postRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
     @DisplayName("게시글을 단건 조회할 수 있다")
     @Test
     void getPostTest() throws Exception {
-        mockMvc.perform(get("/posts/1"))
+        User user = userRepository.save(createUser());
+        Long postId = getPostId(user);
+
+        mockMvc.perform(get("/posts/{id}", postId))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(document("post-get",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("id").description("게시글 번호")
+                        ),
+                        responseFields(
+                                fieldWithPath("title").description("제목"),
+                                fieldWithPath("content").description("내용"),
+                                fieldWithPath("created_at").description("작성 날짜"),
+                                fieldWithPath("created_by").description("작성자")
+                        )
+                ));
+    }
+
+    @DisplayName("게시글을 페이징 조회할 수 있다")
+    @Test
+    void getPostById() throws Exception {
+        User user = userRepository.save(createUser());
+        Long postId = getPostId(user);
+
+        mockMvc.perform(get("/posts"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("post-get-list",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                fieldWithPath("[].title").description("제목"),
+                                fieldWithPath("[].content").description("내용"),
+                                fieldWithPath("[].created_at").description("작성 날짜"),
+                                fieldWithPath("[].created_by").description("작성자")
+                        )
+                ));
+    }
+
+    @DisplayName("게시글을 검색 조회할 수 있다")
+    @Test
+    void getPostBySearchWord() throws Exception {
+        User user = userRepository.save(createUser());
+        Long postId = getPostId(user);
+
+        String searchWord = "짱구는 못말려";
+
+        mockMvc.perform(get("/posts/search?searchWord=" + searchWord))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("post-get-search",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        queryParameters(
+                                parameterWithName("searchWord").description("검색어")
+                        ),
+                        responseFields(
+                                fieldWithPath("[].title").description("제목"),
+                                fieldWithPath("[].content").description("내용"),
+                                fieldWithPath("[].created_at").description("작성 날짜"),
+                                fieldWithPath("[].created_by").description("작성자")
+                        )
+                ));
     }
 
     @DisplayName("게시글을 생성할 수 있다")
     @Test
-    void createPostTest() throws Exception {
-        PostRequest createPostRequest = new PostRequest("제목3","안녕하세요");
+    void createPost() throws Exception {
+        User user = userRepository.save(createUser());
+        Long postId = getPostId(user);
+
+        PostRequest createPostRequest = new PostRequest("자기소개","안녕하세요 홍승근입니다.");
+
+        MockHttpSession session = createSession(user.getId());
 
         mockMvc.perform(post("/posts")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(createPostRequest)))
+                        .content(objectMapper.writeValueAsString(createPostRequest))
+                        .session(session))
                 .andDo(print())
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andDo(document("post-create",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("title").description("제목"),
+                                fieldWithPath("content").description("내용")
+                        )
+                ));
     }
 
     @DisplayName("게시글을 수정할 수 있다")
     @Test
-    void updatePostTest() throws Exception {
-        PostRequest updatePostRequest = new PostRequest("제목3", "안녕하세요 반갑습니다");
+    void updatePost() throws Exception {
+        User user = userRepository.save(createUser());
+        Long postId = getPostId(user);
 
-        mockMvc.perform(patch("/posts/2")
+        PostRequest updatePostRequest = new PostRequest("짱구는 정말로 못말려", "3편에서 계속됩니다");
+
+        MockHttpSession session = createSession(user.getId());
+
+        mockMvc.perform(patch("/posts/{id}",postId)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(updatePostRequest)))
+                        .content(objectMapper.writeValueAsString(updatePostRequest))
+                        .session(session))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(document("post-update",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("id").description("게시글 번호")
+                        ),
+                        requestFields(
+                                fieldWithPath("title").description("제목"),
+                                fieldWithPath("content").description("내용")
+                        )
+                ));
+    }
+
+    @DisplayName("게시글을 삭제할 수 있다")
+    @Test
+    void deletePost() throws Exception {
+        User user = userRepository.save(createUser());
+        Long postId = getPostId(user);
+
+        MockHttpSession session = createSession(user.getId());
+
+        mockMvc.perform(delete("/posts/{id}",postId)
+                        .session(session))
+                .andDo(print())
+                .andExpect(status().isNoContent())
+                .andDo(document("post-delete",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("id").description("게시글 번호")
+                        )
+                ));
+    }
+
+    private MockHttpSession createSession(Long id) {
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("SESSION-KEY", id);
+        return session;
+    }
+
+    private User createUser(){
+        return new User("홍승근", "취미", 25, "test1@naver.com", "1234");
+    }
+
+    private Long getPostId(User user){
+        Post post1 = new Post("짱구는 못말려", "2편에서 계속", user);
+        Post post2 = new Post("짱구는 정말 못말려", "3편에서 계속", user);
+
+        List<Post> posts = postRepository.saveAll(List.of(post1, post2));
+
+        return posts.get(0).getId();
     }
 }
--- src/test/java/com/board/application/user/controller/UserControllerTest.java
+++ src/test/java/com/board/application/user/controller/UserControllerTest.java
@@ -1,21 +1,29 @@
 package com.board.application.user.controller;
 
 import com.board.application.user.dto.CreateUserRequest;
+import com.board.application.user.dto.LoginUserRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.snippet.Attributes;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 public class UserControllerTest {
     @Autowired
     private UserController userController;
@@ -24,51 +32,54 @@ public class UserControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @DisplayName("유저를 생성할 수 있다")
+    @DisplayName("유저는 회원가입 할 수 있다")
     @Test
-    void createUserSuccessTest() throws Exception {
-        CreateUserRequest createUserRequest = new CreateUserRequest("홍승근", "운동", 25, "123@123.com", "123123");
-
-        mockMvc.perform(post("/users")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(createUserRequest)))
-                .andDo(print())
-                .andExpect(status().isCreated());
-    }
-
-    @DisplayName("이름이 빈 값인 유저는 생성할 수 없다")
-    @Test
-    void createUserNameNullTest() throws Exception {
-        CreateUserRequest createUserRequest = new CreateUserRequest("", "운동", 25, "123@123.com", "123123");
+    void createUser() throws Exception {
+        CreateUserRequest createUserRequest = new CreateUserRequest("홍승근", "운동", 25, "test2@naver.com", "1234");
 
         mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(createUserRequest)))
                 .andDo(print())
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isCreated())
+                .andDo(document("user-create",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("name").description("이름"),
+                                fieldWithPath("hobby").optional().description("취미"),
+                                fieldWithPath("age").description("나이"),
+                                fieldWithPath("email").description("이메일").attributes(Attributes.key("constraint").value("길이 20 이하")),
+                                fieldWithPath("password").description("비밀번호").attributes(Attributes.key("constraint").value("길이 20 이하"))
+                        )
+                ));
     }
 
-    @DisplayName("나이가 0인 유저는 생성할 수 없다")
+    @DisplayName("유저는 로그인 할 수 있다")
     @Test
-    void createUserAgeZeroTest() throws Exception {
-        CreateUserRequest createUserRequest = new CreateUserRequest("홍승근", "운동", 0, "123@123.com", "123123");
+    void loginUser() throws Exception {
+        createUserSample();
 
-        mockMvc.perform(post("/users")
+        LoginUserRequest loginUserRequest = new LoginUserRequest( "test1@naver.com", "1234");
+
+        mockMvc.perform(post("/users/login")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(createUserRequest)))
+                        .content(objectMapper.writeValueAsString(loginUserRequest)))
                 .andDo(print())
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isOk())
+                .andDo(document("user-login",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("email").description("이메일").attributes(Attributes.key("constraint").value("길이 20 이하")),
+                                fieldWithPath("password").description("비밀번호").attributes(Attributes.key("constraint").value("길이 20 이하"))
+                        )
+                ));
     }
 
-    @DisplayName("취미가 없는 유저는 생성할 수 있다")
-    @Test
-    void createUserHobbyNullTest() throws Exception {
-        CreateUserRequest createUserRequest = new CreateUserRequest("홍승근", null, 25, "123@123.com", "123123");
-
-        mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(createUserRequest)))
-                .andDo(print())
-                .andExpect(status().isCreated());
+    private void createUserSample(){
+        CreateUserRequest createUserRequest = new CreateUserRequest("홍승근", "운동", 25, "test1@naver.com", "1234");
+        userController.createUser(createUserRequest);
     }
+
 }
